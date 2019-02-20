9a2bff4cf2adbe1ef6e92c40d46067845775a5dd
Shakil Ali <sali011@gold.ac.uk>
2019-02-20 19:50:54 +0000

    MVP Service Requester Completed

--- LaboraServiceRequester/app/src/main/java/com/labora/laboraservicerequester/Services.java
+++ LaboraServiceRequester/app/src/main/java/com/labora/laboraservicerequester/Services.java
@@ -1,23 +1,37 @@
 package com.labora.laboraservicerequester;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firestore.v1.FirestoreGrpc;
 
-public class Services extends AppCompatActivity
+import java.util.HashMap;
+import java.util.Map;
+
+public class Services extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
     // Initialise variables
     private EditText name, postC, phone, email, job, keyW;
     private Button request;
+    private FirebaseFirestore nFirestore;
 
     DatabaseReference databaseReference;
 
@@ -28,6 +42,14 @@ public class Services extends AppCompatActivity
         setContentView(R.layout.activity_services);
 
         // Assignment operations
+        nFirestore = FirebaseFirestore.getInstance();
+
+        final Spinner spinner = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.services, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
         name = (EditText) findViewById(R.id.name);
         postC = (EditText) findViewById(R.id.postC);
         phone = (EditText) findViewById(R.id.phone);
@@ -43,12 +65,44 @@ public class Services extends AppCompatActivity
         request.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addRequests();
+                //addRequests();
+                String requesterService = spinner.getSelectedItem().toString();
+                String requesterName = name.getText().toString();
+                String requesterPostCode = postC.getText().toString();
+                String requesterPhone = phone.getText().toString();
+                String requesterEmail = email.getText().toString();
+                String requesterJob = job.getText().toString();
+                String requesterKeyWord = keyW.getText().toString();
+
+                // Initialise Hash Map
+                Map<String, String> userMap = new HashMap<>();
+                userMap.put("Service", requesterService);
+                userMap.put("Name", requesterName);
+                userMap.put("Post Code", requesterPostCode);
+                userMap.put("Phone", requesterPhone);
+                userMap.put("Email", requesterEmail);
+                userMap.put("Job Description", requesterJob);
+                userMap.put("Keywords", requesterKeyWord);
+
+                nFirestore.collection("Users-Requester").add(userMap).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(Services.this, "Completed", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        String error = e.getMessage();
+                        Toast.makeText(Services.this, "Error" + error, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
             }
         });
 
     }
 
+    /*
     public void addRequests()
     {
         String requesterName = name.getText().toString();
@@ -87,5 +141,16 @@ public class Services extends AppCompatActivity
         }
 
     }
+    */
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
